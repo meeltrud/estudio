22a4a075c85981d86b877c8e498e949af35b4816
meeltrud <70931007+meeltrud@users.noreply.github.com>
2020-10-14 19:18:02 -0300

    Add files via upload

---
+++ TP5/script.js
@@ -0,0 +1,18 @@
+let a = 1
+var b = 2
+var c = [1, 2, 3]
+
+function ola() {
+    if (a == a){
+        console.log("hola")
+    }
+    else{
+        alert("nada que ver")
+    }
+}
+
+function letras(){
+    for(i = 0; i <= 3; i++){
+        console.log(i)
+    }
+}
